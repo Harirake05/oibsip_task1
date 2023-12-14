742ef32e5b5287137b12f090c65ff80b0d93e894
Harirake05 <153834553+Harirake05@users.noreply.github.com>
2023-12-14 19:36:23 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+// You can add any JavaScript code here for interactivity, animations, or dynamic content.
+// For example, you can create functions to open project details or handle form submission.
+
+function openProject(projectName) {
+    // Implement your logic to open project details or perform any desired action.
+    console.log("Opening project: " + projectName);
+  }
+  
